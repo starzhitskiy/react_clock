5bbe034f106f80b7ff336a7ce20e3ddeb3cb70b2
Vlad Starzhitskiy <study.html.css.java@gmail.com>
2021-05-24 17:17:53 +0300

    Updates

---
+++ precache-manifest.35dd9039305b18d55630f229b3b63a66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f630f59f9a0a57ae193728aab362e4f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9cea898fbe625add230a",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cea898fbe625add230a",
+    "url": "./static/js/main.862c21b8.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
